7d4b427028fc05f5670e1a632bb2fbabbd88af48
Marcin <marcinkukulka@gmail.com>
2022-05-08 21:15:12 +0200

    Create layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.css";
+import ThemeProvider from "react-bootstrap/ThemeProvider";
+import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider>
+			<Layout>
+				{" "}
+				<Component {...pageProps} />
+			</Layout>
+		</ThemeProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;

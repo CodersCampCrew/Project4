241c101bad2923a0c1e3fee88e60f0e07834246a
Przemysław Gajowczyk <przemyslaw.gajowczyk23@gmail.com>
2022-05-10 19:12:04 +0200

    setting up mongo db and connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import 'dotenv/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
+const { DB_CONNECT } = process.env;
+
 @Module({
-  imports: [AuthModule],
+  imports: [MongooseModule.forRoot(DB_CONNECT), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })

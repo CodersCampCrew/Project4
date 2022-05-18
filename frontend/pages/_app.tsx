2059a8ee8ed5530908e92a4618f3bbf0c7d3dfa5
Nilphym <nilphym@gmail.com>
2022-05-18 18:49:49 +0200

    Refactor auth slice

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
-import store from "../components/store/index";
+import store from "../store/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
